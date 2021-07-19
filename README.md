0684b55fe0ad3f447383533112651732462497e1
Alice <Alice@LAPTOP-GALHPM23>
2021-07-19 16:15:59 +0200

    ...

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-#Placeholder
+# Alice-Secur-Bot-Discord
+
+Ceci est un bot qui va sécuriser votre discord des fichiers potentiellement malveillants en suppriment des fichiers du type ``.exe`` ou ``.bat`` (à configurer en cas de modification dans le [/main.js](https://github.com/SaabniaTv/)) ^^
+
+## Avant toute chose !
+
+Installer bien [Discord JS](https://discord.js.org/#/) sur votre projet sinon le bot de marchera jamais !!
+
+### prérequis
+
+- Un bot avec un token [Discord Developper Application](https://discord.com/developers/applications/726766282354982912/information) (oui oui, je vous jure ^^)
+- Discord JS = [Discord JS](https://discord.js.org/#/)
+- Node JS = [Node JS](https://nodejs.org/en/)
+
+### 1er démarrage
+
+Aller dans le fichier [/config.json](https://github.com/SaabniaTv/), et remplacer "YOUR BOT TOKEN YEAAAHHHHHHHH <3 UwU" par votre bot Token !
+
+-> Dans un terminal noter = ``node main``
+
+## Author
+
+* **Alice** _alias_ [@SaabniaTv](https://github.com/SaabniaTv)
+
--- main.js
+++ main.js
@@ -1,13 +1,23 @@
+const config = require("./config.json");
 const Discord = require("discord.js");
-const client = new Discord.Client();
-//Ci-Dessous les extentions de fichier blacklist ^^
+const fs = require("fs");
+const bot = new Discord.Client({ disableEveryone: true });
+
+
+// Ci-Dessous les extentions de fichier blacklist ^^
 const blacklist = ['exe', 'vbs', 'bat'];
+
+// Petite initialisation du bot
 bot.on("ready", () => {
+    // Ici on envoie un message dans la console pour dire le nom enregistrer sur votre panel + on indique qu'il est en ligne UwU
     console.log(`${bot.user.username} est en ligne !`);
+    // On affiche une activité pour le bot (joue à.....)
     bot.user.setActivity("by SaabniaTv | https://github.com/SaabniaTv/");
 });
 
-client.on("message", (message) => {
+
+
+bot.on("message", (message) => {
     if (message.attachments.size > 0) {
         // Cette constante divise l'url avec chaque . et prend le dernier objet dans le tableau avec le .pop pour que les gens ne puissent pas tricher en nommant leurs fichiers thing.txt.exe. 
         const MIME = message.attachments.map(a => a.url.split('.').pop())
@@ -20,11 +30,12 @@ client.on("message", (message) => {
             if (message.member.hasPermission('VIEW_AUDIT_LOG')) return console.log(message.author + ' (' + message.author.username + ')' + ' sent a (probably) malicious file but has VIEW_AUDIT_LOG permission so it was ignored.')
             message.delete()
             // Ici le Bot renvoie un message dans le Chanel pour le "demander pourquoi ce fichier" est pour le lui dire que le fichier n'est pas accepté sur le serveur :)))))
-            message.reply(' Qu est - ce que vous essayez de faire ?' + LowerCStringMIME + 'file' + ' non accepté sur ce serveur for security reasons!)')
+            message.reply(' Qu est - ce que vous essayez de faire ? ' + LowerCStringMIME + 'file' + ' non accepte sur ce serveur !')
             // Le bot renvoie dans la console le pseudo discord de la personne + un message indiquant qu il a essayé d'envoyer un fichier ou l'extension et interdite sur le serveur
-            console.log(message.author + ' (' + message.author.username + ')' + ' a essayé d envoyer un fichier(probablement) malveillant!')
+            console.log(message.author + ' (' + message.author.username + ')' + ' a essaye d envoyer un fichier probablement malveillant!')
         }
     }
 });
 
-client.login("YOUR BOT TOKEN !");
+
+bot.login(config.token);
